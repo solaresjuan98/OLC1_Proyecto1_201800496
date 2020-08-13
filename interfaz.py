03bda09a46383892fd98bbf466553f096ea9d484
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-08-12 20:28:00 -0600

    commit

--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,28 @@
 from tkinter import *
 #from tkinter import messasebox as MessageBox
 from io import open
+from tkinter import filedialog
+
+# función para abrir archivo de texto
+
+textoArchivo = ""
+def abrirArchivo():
+    # ABRIENDO ARCHIVO
+    filename = filedialog.askopenfilename(
+        initialdir="c://", title="Seleccionar archivo",
+        filetypes=(("archivos javascript", "*.js"), ("archivos HTML", "*.html"), ("archivos CSS", "*.css")))
+    # imprime la ruta
+    ruta = filename
+    print(ruta)
+
+    try:
+        with open(filename, 'r') as UseFile:
+            textoArchivo = UseFile.read()
+            textoEntrada.insert(INSERT, textoArchivo)
+            #print(UseFile.read())
+
+    except:
+        print("El archivo no existe.")
 
 
 ventana = Tk()
@@ -11,8 +33,7 @@ ventana.resizable(False, False)
 miFrame = Frame(ventana, width=950, height=600, bg="#292D3E")
 miFrame.pack()
 
-### BARRA DE MENÚ
-
+# BARRA DE MENÚ
 # insetar barra de menu
 barraMenu = Menu(ventana)
 ventana.config(menu=barraMenu)
@@ -20,7 +41,7 @@ ventana.config(menu=barraMenu)
 filemenu = Menu(barraMenu)
 filemenu = Menu(barraMenu, tearoff=0)
 filemenu.add_command(label="Nuevo")
-filemenu.add_command(label="Abrir")
+filemenu.add_command(label="Abrir", command=abrirArchivo)
 filemenu.add_command(label="Guardar")
 filemenu.add_command(label="Guardar como...")
 filemenu.add_separator()
@@ -40,20 +61,27 @@ barraMenu.add_cascade(label="Analizar", menu=analizar)
 barraMenu.add_cascade(label="Reportes", menu=reportes)
 
 
-### COMPONENTES QUE VAN DENTRO DEL FRAME
+# COMPONENTES QUE VAN DENTRO DEL FRAME
 labelTitulo = Label(ventana, text="Editor de texto")
-labelTitulo.place(x=10, y =10)
+labelTitulo.place(x=10, y=10)
 labelTitulo.config(padx=5, pady=5, font=("sans-serif", 10))
 
 textoEntrada = Text(ventana)
 textoEntrada.place(x=10, y=60)
 textoEntrada.config(width=65, height=35, font=("Consolas", 9),
-    padx=5, pady=5)
-
+                    padx=5, pady=5)
+textoEntrada.insert(INSERT, textoArchivo)
 
 textoSalida = Text(ventana)
 textoSalida.place(x=505, y=60)
 textoSalida.config(width=60, height=35, font=("Consolas", 9),
-    padx=5, pady=5)
+                   padx=5, pady=5, state=DISABLED)
+
+
+# BOTONES
+botonBorrar = Button(ventana, text="Borrar")
+botonBorrar.place(x=850, y=10)
+botonBorrar.config(padx=3, pady=3, font=("sans-serif", 10))
+
 
 ventana.mainloop()
